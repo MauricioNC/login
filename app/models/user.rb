eef792fdc27cee0c662db363818b99cd506cb655
MauricioNC <mauricionc19@gmailc.om>
2022-12-27 21:23:52 -0600

    Wass corrected a typo in the routes and validations for the User in the model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,19 @@
 class User < ApplicationRecord
   validates :name, length: {minimum:3, maximum:15}
-  validates :name, presence: true, on: :create, message: "Name can't be blank"
-  validates :name, unique: true, on: :create, message: "Name must be unique"
+  validates_presence_of :name, presence: true, on: :create, message: "Name can't be blank"
+  validates_uniqueness_of :name, unique: true, on: :create, message: "Name must be unique"
 
   validates :username, length: {minimum:8, maximum:16}
-  validates :username, presence: true, on: :create, message: "Username can't be blank"
-  validates :username, unique: true, on: :create, message: "Username must be unique"
+  validates_presence_of :username, presence: true, on: :create, message: "Username can't be blank"
+  validates_uniqueness_of :username, unique: true, on: :create, message: "Username must be unique"
 
   validates :password_digest, length: {minimum:8, maximum:16}
-  validates :password_digest, presence: true, on: :create, message: "Password can't be blank"
-  validates :password_digest, unique: true, on: :create, message: "Password must be unique"
+  validates_presence_of :password_digest, presence: true, on: :create, message: "Password can't be blank"
+  validates_uniqueness_of :password_digest, unique: true, on: :create, message: "Password must be unique"
 
   validates :password_confirmation, presence: true
-  validates :password_confirmation, presence: true, on: :create, message: "Password confirmation can't be blank"
-  validates :password_confirmation, unique: true, on: :create, message: "Password confirmation must be unique"
+  validates_presence_of :password_confirmation, presence: true, on: :create, message: "Password confirmation can't be blank"
+  validates_uniqueness_of :password_confirmation, unique: true, on: :create, message: "Password confirmation must be unique"
+
+  has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,3 +2,4 @@ Rails.application.routes.draw do
   root 'home#index'
 
   resources :users, only: [:new, :create]
+end
